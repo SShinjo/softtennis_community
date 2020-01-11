acdbb790d36502af55d62b624730aa47619c0093
SShinjo <skskosmn@gmail.com>
2020-01-11 09:13:11 +0000

    #10

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,14 @@
 class ApplicationController < ActionController::Base
+	before_action :configure_permitted_parameters, if: :devise_controller?
+	protect_from_forgery
+
+	protected
+		def configure_permitted_parameters
+			devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :positon, :level])
+		end
+
+	private
+		def after_sign_in_path_for(resource)
+			user_path(resource)
+		end
 end
