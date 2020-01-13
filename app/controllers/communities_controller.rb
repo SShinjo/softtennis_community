34f932f9e1d6c9a9d5d8d5fcd2797664fde9b547
SShinjo <skskosmn@gmail.com>
2020-01-13 12:11:59 +0000

    #16

--- app/controllers/communities_controller.rb
+++ app/controllers/communities_controller.rb
@@ -1,10 +1,42 @@
 class CommunitiesController < ApplicationController
+
   def new
+  	@com = Community.new
+    @com.members.build
+  end
+
+  def create
+    @com = Community.create(community_params)
+  	redirect_to communities_path
   end
 
   def index
+  	@coms = Community.active.yet
+    @coms.each do |com|
+      @member = com.members.build
+    end
   end
 
   def edit
+  	@com = Community.find(params[:id])
   end
+
+  def update
+  	@com = Community.find(params[:id])
+  	@com.update
+  	redirect_to communities_path
+  end
+
+  def destroy
+  	@com = Community.find(params[:id])
+  	@com.destroy
+  	redirect_to(current_user)
+  end
+
+  private
+  	def community_params
+  		params.require(:community).permit(:name, :introduction, :date, :time, :place,
+  											:capacity, :is_closed, :is_held, 
+                        members_attributes: [:id, :user_id, :community_id, :is_host, :is_member])
+  	end
 end
--- app/models/member.rb
+++ app/models/member.rb
@@ -1,2 +1,7 @@
 class Member < ApplicationRecord
+	belongs_to :user
+	belongs_to :community
+
+	# コミュニティを作成したユーザー＝ホスト
+	scope :host, -> { where(is_host: true) }
 end
